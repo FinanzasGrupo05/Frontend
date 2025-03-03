62cb53371014f726ee59140fb89379fadb02d4ef
DanielChirinos615 <89100997+DanielChirinos615@users.noreply.github.com>
2025-03-03 01:33:15 -0500

    final

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL: "https://app-250303000531.azurewebsites.net/",
     headers: {
         "Content-type": "application/json"
     }
