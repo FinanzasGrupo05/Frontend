d8b6a0cc13d2397cc117b833d8f40dc9f4525bd0
Nat2cc <natalia301812@gmail.com>
2025-03-04 19:54:54 -0500

    listooooo

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export default axios.create({
-    //baseURL: "https://app-250303011850.azurewebsites.net/",
-    baseURL: "http://localhost:8080/",
+    baseURL: "https://app-250303011850.azurewebsites.net/",
+    //baseURL: "http://localhost:8080/",
     headers: {
         "Content-type": "application/json"
     }
