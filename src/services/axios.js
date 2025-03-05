e6f0784cf42b35997dd401261dc05cc8356d1257
Nat2cc <natalia301812@gmail.com>
2025-03-04 19:52:12 -0500

    aaa

--- src/services/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-export default axios.create({
-    baseURL: "https://app-250303011850.azurewebsites.net/",
-    headers: {
-        "Content-type": "application/json"
-    }
-})
--- src/services/users/register.service.js
+++
@@ -1,4 +0,0 @@
-import axios from "@/services/axios.js";
-export default async function ({username, password, name, sex}) {
-    return ( await axios.post('/user', {name, sex, username, password}) ).data
-}
